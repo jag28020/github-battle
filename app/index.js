d546ff9bbdfef0e1624831dd13f5da43779bb791
Jake Greenstein <jag28020@gmail.com>
2016-08-18 20:19:38 -0400

    initial commit

---
+++ app/index.js
@@ -0,0 +1,28 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+var routes = require('./config/routes')
+var Raven = require('raven-js')
+
+var sentryKey = '153abc44c316401ca9a665a6a48982a8'
+var sentryApp = '92533'
+var sentryURL = 'https://'+sentryKey+'@app.getsentry.com/'+sentryApp
+
+
+var _APP_INFO = {
+	name: 'Github Battle',
+	branch: 'video4',
+	version: '1.0'
+}
+
+
+Raven.config(sentryURL, {
+	release: _APP_INFO.version,
+	tags: {
+		branch: _APP_INFO.branch,
+	}
+}).install()
+
+
+
+
+ReactDOM.render(routes, document.getElementById('app'))
